98dde1dd2c331c0068202d027d7990739ef0556e
flfer <florian.ferrier@tutamail.com>
2023-01-22 12:09:05 +0100

    Add UART Tx to send data

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -4,7 +4,13 @@
 
 #include "led.h"
 
+#include <string.h>
+
+UART_HandleTypeDef huart2;
+DMA_HandleTypeDef hdma_usart2_tx;
+
 void SystemClock_Config(void);
+void USER_UART_Init(void);
 
 int main(void)
 {
@@ -12,11 +18,20 @@ int main(void)
 
     SystemClock_Config();
 
+    USER_UART_Init();
+
     BaseType_t result = 0;
     result = xTaskCreate (USER_LED_Task, "Task_Led_Blink", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
     if(pdPASS != result)
     {
         Error_Handler();
+    }
+
+    const char pData[] = "Hello world\r\n";
+    size_t sizeData = strlen(pData);
+    if(HAL_OK != HAL_UART_Transmit(&huart2, (uint8_t*)pData, (uint16_t)sizeData, 1000))
+    {
+        // Nothing
     }
 
     vTaskStartScheduler();
@@ -66,6 +81,59 @@ void SystemClock_Config(void)
     }
 }
 
+void USER_UART_Init(void)
+{
+    /* GPIO Initialization */
+    __HAL_RCC_USART2_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**USART2 GPIO Configuration
+        PA2     ------> USART2_TX
+        PA3     ------> USART2_RX
+    */
+    GPIO_InitTypeDef GPIO_InitStruct = {
+        .Pin = GPIO_PIN_2 | GPIO_PIN_3,
+        .Mode = GPIO_MODE_AF_PP,
+        .Pull = GPIO_NOPULL,
+        .Speed = GPIO_SPEED_FREQ_VERY_HIGH,
+        .Alternate = GPIO_AF7_USART2,
+    };
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    /* UART Initialization */
+    huart2.Instance = USART2;
+    huart2.Init.BaudRate = 115200;
+    huart2.Init.WordLength = UART_WORDLENGTH_8B;
+    huart2.Init.StopBits = UART_STOPBITS_1;
+    huart2.Init.Parity = UART_PARITY_NONE;
+    huart2.Init.Mode = UART_MODE_TX;
+    huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+    if(HAL_OK != HAL_UART_Init(&huart2))
+    {
+        Error_Handler();
+    }
+    HAL_NVIC_SetPriority(USART2_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(USART2_IRQn);
+
+    /* DMA Initialization */
+    hdma_usart2_tx.Instance = DMA1_Channel7;
+    hdma_usart2_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
+    hdma_usart2_tx.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_usart2_tx.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_usart2_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+    hdma_usart2_tx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+    hdma_usart2_tx.Init.Mode = DMA_NORMAL;
+    hdma_usart2_tx.Init.Priority = DMA_PRIORITY_LOW;
+    if(HAL_OK != HAL_DMA_Init(&hdma_usart2_tx))
+    {
+        Error_Handler();
+    }
+    __HAL_LINKDMA(&huart2, hdmatx, hdma_usart2_tx);
+    __HAL_RCC_DMA1_CLK_ENABLE();
+    HAL_NVIC_SetPriority(DMA1_Channel7_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(DMA1_Channel7_IRQn);
+}
+
 void Error_Handler(void)
 {
     /* User can add his own implementation to report the HAL error return state */
@@ -74,6 +142,16 @@ void Error_Handler(void)
     {
         // Nothing
     }
+}
+
+void DMA1_Channel7_IRQHandler(void)
+{
+    HAL_DMA_IRQHandler(&hdma_usart2_tx);
+}
+
+void USART2_IRQHandler(void)
+{
+    HAL_UART_IRQHandler(&huart2);
 }
 
 #ifdef  USE_FULL_ASSERT
