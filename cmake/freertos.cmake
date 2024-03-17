7da0426fc3be493f8fb9b2b02538a5a4aee7541d
ferrierf <florian.ferrier@aol.fr>
2024-03-17 12:08:27 +0100

    Fix include FreeRTOSConfig.h on cmake

--- cmake/freertos.cmake
+++ cmake/freertos.cmake
@@ -1,10 +1,13 @@
 include(FetchContent)
 
+set(RTOS_DIRECTORY ${CMAKE_SOURCE_DIR}/lib/rtos CACHE STRING "" FORCE)
+set(FREERTOS_CONFIG_FILE_DIRECTORY ${RTOS_DIRECTORY} CACHE STRING "" FORCE)
+
 FetchContent_Declare(
     freertos_kernel
     GIT_REPOSITORY https://github.com/FreeRTOS/FreeRTOS-Kernel.git
     GIT_TAG V10.5.1
-    SOURCE_DIR ${CMAKE_SOURCE_DIR}/lib/rtos/freertos_kernel
+    SOURCE_DIR ${RTOS_DIRECTORY}/freertos_kernel
 )
 
 FetchContent_MakeAvailable(freertos_kernel)
