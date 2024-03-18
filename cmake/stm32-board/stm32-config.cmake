aadece687010a60428a537bb97cd8722af6b0e78
ferrierf <florian.ferrier@aol.fr>
2024-03-18 16:02:13 +0100

    Add stm32f100rbtx mcu support

--- cmake/stm32-board/stm32-config.cmake
+++ cmake/stm32-board/stm32-config.cmake
@@ -2,4 +2,5 @@ list(APPEND list_available_board
     stm32l152retx
     stm32f446retx
     stm32f103c8t6
+    stm32f100rbtx
 )
---
+++ cmake/stm32-board/stm32f1/stm32f100rbtx/stm32f100rbtx-config.cmake
@@ -0,0 +1,3 @@
+set(SPECIFIC_BOARD_HAL STM32F100xB)
+set(LINKER_FILE ${CMAKE_CURRENT_SOURCE_DIR}/cmake/stm32-board/stm32f1/stm32f100rbtx/stm32f100rbtx.ld)
+set(STARTUP_FILE startup_stm32f100xb.s)
